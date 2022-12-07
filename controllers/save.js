babff11c70df7b05e104ca0e08d13f8938fa5064
HKong31 <113340082+HKong31@users.noreply.github.com>
2022-12-07 08:33:14 -0400

    Fix crash when voice message recieved
    Fix crash si recibe nota de voz

--- controllers/save.js
+++ controllers/save.js
@@ -8,8 +8,14 @@ const fs = require('fs')
 
 
 const saveMedia = (media) => {
-    const extensionProcess = mimeDb[media.mimetype]
-    const ext = extensionProcess.extensions[0]
+    const extensionProcess = mimeDb[media.mimetype];
+    let ext;
+    if (!extensionProcess) {
+        const fileType = media.mimetype.split('/');
+        ext = fileType[1].split(';')[0];
+    } else {
+        ext = extensionProcess.extensions[0];
+    }
     fs.writeFile(`./media/${Date.now()}.${ext}`, media.data, { encoding: 'base64' }, function (err) {
         console.log('** Archivo Media Guardado **');
     });
