99f508f93889d70240861158bc304c25a3b2daef
cheveguerra <cheveguerra@gmail.com>
2022-12-14 21:07:59 -0600

    feat: correccion de flujos en app.js de ejemplo
    Se corrigio una variable que se mostraba del objeto ctx que no existe en todos los proveedores

--- starters/apps/base-venom-mysql/app.js
+++ starters/apps/base-venom-mysql/app.js
@@ -3,21 +3,74 @@ const {
     createProvider,
     createFlow,
     addKeyword,
+    addChild,
 } = require('@bot-whatsapp/bot')
 
-const VenomProvider = require('@bot-whatsapp/provider/venom')
+const WebWhatsappProvider = require('@bot-whatsapp/provider/web-whatsapp')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'HOLA'])
-    .addAnswer('Bienvenido a mi tienda')
+/**
+ * Declarando flujo hijo
+ */
+
+const flowBolsos2 = addKeyword(['bolsos2', '2'])
+    .addAnswer('🤯 *MUCHOS* bolsos ...')
+    .addAnswer('y mas bolsos... bla bla')
+
+const flowZapatos2 = addKeyword(['zapatos2', '2'])
+    .addAnswer('🤯 repito que tengo *MUCHOS* zapatos.')
+    .addAnswer('y algunas otras cosas.')
+
+const flowZapatos = addKeyword(['zapatos', 'ZAPATOS'])
+    .addAnswer('🤯 Veo que elegiste zapatos')
+    .addAnswer('Tengo muchos zapatos...bla bla')
+    .addAnswer(
+        ['Manda:', '*2*', 'o', '*zapatos2*', 'para mas información'],
+        { capture: true },
+        (ctx) => {
+            console.log('Aqui puedes ver más info del usuario...')
+            console.log('Puedes enviar un mail, hook, etc..')
+            console.log(ctx)
+        },
+        [...addChild(flowZapatos2)]
+    )
+
+const flowBolsos = addKeyword(['bolsos', 'BOLSOS'])
+    .addAnswer('🙌 Veo que elegiste bolsos')
+    .addAnswer('Tengo muchos bolsos...bla bla')
+    .addAnswer(
+        ['Manda:', '*2*', 'o', '*bolsos2*', 'para mas información.'],
+        { capture: true },
+        (ctx) => {
+            console.log('Aqui puedes ver más info del usuario...')
+            console.log('Puedes enviar un mail, hook, etc..')
+            console.log(ctx)
+        },
+        [...addChild(flowBolsos2)]
+    )
+
+/**
+ * Declarando flujo principal
+ */
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('Hola, bienvenido a mi tienda')
     .addAnswer('Como puedo ayudarte?')
     .addAnswer(['Tengo:', 'Zapatos', 'Bolsos', 'etc..'])
+    .addAnswer(
+        ['Para continuar escribe:', '*Zapatos*', 'o', '*Bolsos*'],
+        { capture: true },
+        (ctx) => {
+            console.log('Aqui puedes ver más info del usuario...')
+            console.log('Puedes enviar un mail, hook, etc..')
+            console.log(ctx)
+        },
+        [...addChild(flowBolsos), ...addChild(flowZapatos)]
+    )
 
 const main = async () => {
     const adapterDB = new MockAdapter()
     const adapterFlow = createFlow([flowPrincipal])
-    const adapterProvider = createProvider(VenomProvider)
-
+    const adapterProvider = createProvider(WebWhatsappProvider)
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
