b6bf43d70fc28c6a229522b9b0de76cec43ac864
Leifer Mendez <leifer33@gmail.com>
2022-12-19 12:16:20 +0100

    fix(provider): :fire: send message togther with media

--- packages/provider/src/venom/utils.js
+++ packages/provider/src/venom/utils.js
@@ -1,4 +1,7 @@
-const { writeFile } = require('fs')
+const { writeFile, createWriteStream } = require('fs')
+const { tmpdir } = require('os')
+const http = require('http')
+const https = require('https')
 const combineImage = require('combine-image')
 
 const venomCleanNumber = (number, full = false) => {
@@ -35,9 +38,43 @@ const venomGenerateImage = async (base) => {
     cleanImage.write(PATH_QR)
 }
 
+/**
+ * Incompleta
+ * Descargar archivo multimedia para enviar
+ * @param {*} url
+ * @returns
+ */
+const venomDownloadMedia = (url) => {
+    return new Promise((resolve, reject) => {
+        const ext = url.split('.').pop()
+        const checkProtocol = url.includes('https:')
+        const handleHttp = checkProtocol ? https : http
+        const name = `tmp-${Date.now()}.${ext}`
+        const fullPath = `${tmpdir()}/${name}`
+        const file = createWriteStream(fullPath)
+        handleHttp.get(url, function (response) {
+            response.pipe(file)
+            file.on('finish', function () {
+                file.close()
+                resolve(fullPath)
+            })
+            file.on('error', function () {
+                console.log('errro')
+                file.close()
+                reject(null)
+            })
+        })
+    })
+}
+
 const venomisValidNumber = (rawNumber) => {
     const regexGroup = /\@g.us\b/gm
     const exist = rawNumber.match(regexGroup)
     return !exist
 }
-module.exports = { venomCleanNumber, venomGenerateImage, venomisValidNumber }
+module.exports = {
+    venomCleanNumber,
+    venomGenerateImage,
+    venomisValidNumber,
+    venomDownloadMedia,
+}
