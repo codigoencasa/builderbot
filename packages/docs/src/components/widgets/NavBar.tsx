65ffbc358d96d68f0eb9a5e43a507eff07f2b8ff
Leifer Mendez <leifer33@gmail.com>
2022-12-22 13:49:37 +0100

    docs: :memo: update docs

--- packages/docs/src/components/widgets/NavBar.tsx
+++ packages/docs/src/components/widgets/NavBar.tsx
@@ -1,20 +1,28 @@
 import { component$ } from '@builder.io/qwik'
+import { useLocation } from '@builder.io/qwik-city'
 
 /**
  * options = [] array con la lista de opciones de la documentacion
  */
 export default component$(
-    ({
-        options = [],
-    }: {
-        options: { link: string; name: string; class?: string }[]
-    }) => {
+    ({ options = [] }: { options: { link: string; name: string }[] }) => {
+        const location = useLocation()
+        const currentPage = location.pathname
         return (
             <div>
                 <ul>
                     {options.map((opt) => (
-                        <li class={opt.class}>
-                            <a href={opt.link}>{opt.name}</a>
+                        <li>
+                            <a
+                                class={
+                                    currentPage === `${opt.link}/`
+                                        ? 'font-semibold'
+                                        : ''
+                                }
+                                href={opt.link}
+                            >
+                                {opt.name}
+                            </a>
                         </li>
                     ))}
                 </ul>
