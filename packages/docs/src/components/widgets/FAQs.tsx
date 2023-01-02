641d9a25195848152e7233215b08eca26c2f616a
Leifer Mendez <leifer33@gmail.com>
2023-01-02 10:03:01 +0100

    docs(faq): added fat

--- packages/docs/src/components/widgets/FAQs.tsx
+++ packages/docs/src/components/widgets/FAQs.tsx
@@ -5,39 +5,33 @@ export default component$(() => {
     const items = [
         [
             {
-                question: 'What do I need to start?',
-                answer: `Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds.
-
-    Many say exploration is part of our destiny, but it’s actually our duty to future generations.`,
+                question: '¿Que necesitas para iniciar?',
+                answer: `Deseable tener conocimientos previos en JavaScript o ejecución de proyectos Node. La comunidad siempre se encargará de mantener la documentación lo más clara posible para que con solo unos minutos tengas tu chatbot funcionando correctamente`,
             },
             {
-                question: 'How to install the Qwik + Tailwind CSS template?',
-                answer: `Well, the way they make shows is, they make one show. That show's called a pilot.
-
-    Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.`,
+                question: '¿Es Gratis?',
+                answer: `Si. Es un proyecto Open Source que ofrece el código para que puedas aplicarlo de manera totalmente gratuita. Siempre destacando el valor aportado por toda la comunidad`,
             },
             {
                 question:
-                    "What's something that you completely don't understand?",
-                answer: `A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear. I didn't even know we were calling him Big Bear.`,
+                    "¿Funciona en Ubuntu/Windows?",
+                answer: `El proyecto funciona perfectamente Linux/Windows/Mac. Cabe destacar que dependiendo del sistema operativo será necesario realizar algunos ajustes puntuales. En la documentación se explica más estos casos de usos`,
             },
         ],
         [
             {
-                question: "What's an example of when you changed your mind?",
-                answer: `Michael Knight a young loner on a crusade to champion the cause of the innocent. The helpless. The powerless in a world of criminals who operate above the law. Here he comes Here comes Speed Racer. He's a demon on wheels.`,
+                question: "¿Existe un plan de pago?",
+                answer: `Actualmente, no contamos con un plan de pago, las aportaciones económicas recibidas se destinan a gastos en comunes: servidores para pruebas, servicios de api externos, recursos de marketing y diseño, recintos para capacitaciones, entre otras cosas. `,
             },
             {
                 question:
-                    'What is something that you would really like to try again?',
-                answer: `A business big enough that it could be listed on the NASDAQ goes belly up. Disappears!
-
-      It ceases to exist without me. No, you clearly don't know who you're talking to, so let me clue you in.`,
+                    '¿Riesgos dé bloqueo?',
+                answer: `Depende. Esta librería es una capa superior agnóstica al proveedor que facilita el escribir flujos de conversación. Esto significa que la conexión con "Whatsapp" es delegada al proveedor de turno, el cual dependiendo de cuál elijas, puede tener limitaciones o riesgos. Ver más información`,
             },
             {
                 question:
-                    'If you could only ask one question to each person you meet, what would that question be?',
-                answer: `This is not about revenge. This is about justice. A lot of things can change in twelve years, Admiral. Well, that's certainly good to know. About four years. I got tired of hearing how young I looked.`,
+                    '¿Casos de usos?',
+                answer: `Basado en los relatos compartidos por la comunidad, hasta el momento hemos registrado más de 100 casos de usos. Los más populares suelen ser: asistencia técnica. (preguntas y respuestas), gestión de pedidos de restaurantes, chatbot con inteligencia artificial gracias a dialogflow`,
             },
         ],
     ]
