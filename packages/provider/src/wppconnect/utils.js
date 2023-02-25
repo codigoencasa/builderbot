f659fc89e888a4824f70e9f9f3a3274ad4d4f922
Carlos-create32 <78939137+Carlos-create32@users.noreply.github.com>
2023-02-25 10:55:05 -0500

    build(provider): :sparkles: add a new provider in the project

---
+++ packages/provider/src/wppconnect/utils.js
@@ -0,0 +1,40 @@
+const { createWriteStream } = require('fs')
+const { cleanImage } = require('../utils/cleanImage')
+const qr = require('qr-image')
+
+const WppConnectCleanNumber = (number, full = false) => {
+    number = number.replace('@s.whatsapp.net', '')
+    number = !full ? `${number}@s.whatsapp.net` : `${number}`
+    return number
+}
+
+/**
+ * Hace promesa el write
+ * @param {*} base64
+ */
+const WppConnectGenerateImage = async (base64, name = 'qr.png') => {
+    const PATH_QR = `${process.cwd()}/${name}`
+    let qr_svg = qr.image(base64, { type: 'png', margin: 4 })
+
+    const writeFilePromise = () =>
+        new Promise((resolve, reject) => {
+            const file = qr_svg.pipe(createWriteStream(PATH_QR))
+            file.on('finish', () => resolve(true))
+            file.on('error', reject)
+        })
+
+    await writeFilePromise()
+    await cleanImage(PATH_QR)
+}
+
+const WppConnectValidNumber = (rawNumber) => {
+    const regexGroup = /\@g.us\b/gm
+    const exist = rawNumber.match(regexGroup)
+    return !exist
+}
+
+module.exports = {
+    WppConnectValidNumber,
+    WppConnectGenerateImage,
+    WppConnectCleanNumber,
+}
