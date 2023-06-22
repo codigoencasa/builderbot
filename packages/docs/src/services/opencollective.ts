fff26d5634f7a57b5993fcfa907ea4a698776fed
ozzyoss77 <crhistian.riverin@gmail.com>
2023-06-21 22:04:19 -0400

    Adding @bot/provider documentation: meta/twilio/baileys/venom/wppconnect/wweb

--- packages/docs/src/services/opencollective.ts
+++ packages/docs/src/services/opencollective.ts
@@ -2,6 +2,8 @@
  * GET API from OpenCollective
  * @returns
  */
+// envolver listUsers en un try catch y devolver un array vacio en caso de error
+
 export const fetchOpenCollective = async () => {
     const data = await fetch(`https://opencollective.com/bot-whatsapp/members/users.json?limit=22&offset=0`, {
         method: 'GET',
@@ -13,4 +15,15 @@ export const fetchOpenCollective = async () => {
         login: u.name,
         id: u.MemberId,
     }))
+    // try {
+    //     const listUsers = await data.json()
+    //     return listUsers.map((u: any) => ({
+    //         html_url: u.profile,
+    //         avatar_url: u.image ?? 'https://i.imgur.com/HhiYKwN.png',
+    //         login: u.name,
+    //         id: u.MemberId,
+    //     }))
+    // } catch (error) {
+    //     return []
+    // }
 }
