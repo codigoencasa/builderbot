574b0d35d1306c935ccb6490018e7c8dfbb73bbe
Harrison Chumpitaz <hchumpitaz92@gmail.com>
2023-08-08 02:52:48 -0500

    feat(database): Se agrego sp para guardar y actualizar contactos en postgres

--- packages/database/src/postgres/index.js
+++ packages/database/src/postgres/index.js
@@ -44,19 +44,21 @@ class PostgreSQLAdapter {
 
     save = async (ctx) => {
         const values = [
-            ctx.ref, ctx.keyword, ctx.answer, ctx.refSerialize, ctx.from, JSON.stringify(ctx.options), ctx?.name ?? ''
+            ctx.ref, ctx.keyword, ctx.answer, ctx.refSerialize, ctx.from, JSON.stringify(ctx.options)
         ]
-        const query = `SELECT save_or_update_history_and_contact($1, $2, $3, $4, $5, $6, $7)`;
+        const query = `SELECT save_or_update_history_and_contact($1, $2, $3, $4, $5, $6)`;
 
         try {
             await this.db.query(query, values);
+            console.log('🆗 Historico creado con exito');
         } catch (error) {
             console.error('Error al registrar la entrada del historial:', error);
         }
         this.listHistory.push(ctx)
     }
 
-    getContact = async (from) => {
+    getContact = async (ctx) => {
+        const from = ctx.from
         const query = `SELECT * FROM public.contact WHERE phone = $1 LIMIT 1`;
         try {
             const result = await this.db.query(query, [from]);
@@ -68,34 +70,26 @@ class PostgreSQLAdapter {
     }
 
     saveContact = async (ctx) => {
-        const values = [ctx?.name ?? '']
+        // action: u (Actualiza el valor de ctx.values), a (Agrega). Agrega por defecto.
+        const _contact = await this.getContact(ctx)
+        let jsValues = {}
 
-        const contact = await this.getContact(ctx.from)
-
-        if (contact) {
-            const query = `
-                UPDATE contact SET 
-                updated_in = current_timestamp,
-                last_interaction =  current_timestamp,
-                name = $1
-                WHERE id = ${contact.id}`;
-            try {
-                await this.db.query(query, values);
-            } catch (error) {
-                console.error('Error al actualizar contacto:', error);
-                throw error;
-            }
+        if ((ctx?.action ?? 'a') === 'a') {
+            jsValues = {..._contact.values, ...ctx?.values ?? {}}
         } else {
-            const query = `INSERT INTO contact (name, phone) VALUES ($1, $2)`;
-
-            try {
-                await this.db.query(query, values);
-            } catch (error) {
-                console.error('Error al registrar la entrada del contacto:', error);
-            }
+            jsValues = ctx?.values ?? {}
         }
 
-        return await this.getContact(ctx.from)
+        const values = [ctx.from, JSON.stringify(jsValues)]
+        const query = `SELECT save_or_update_contact($1, $2)`;
+
+        try {
+            await this.db.query(query, values);
+            console.log('🆗 Contacto guardado o actualizado con éxito');
+        } catch (error) {
+            console.error('🚫 Error al guardar o actualizar contacto:', error);
+            throw error;
+        }
     }
 
     checkTableExistsAndSP = async () => {
@@ -103,17 +97,18 @@ class PostgreSQLAdapter {
         const contact = `
             CREATE TABLE IF NOT EXISTS contact (
                 id SERIAL PRIMARY KEY,
-                name VARCHAR(255),
                 phone VARCHAR(255) NOT NULL,
                 created_at TIMESTAMP DEFAULT current_timestamp,
                 updated_in TIMESTAMP,
-                last_interaction TIMESTAMP
+                last_interaction TIMESTAMP,
+                values JSONB
             )`;
+
         try {
             await this.db.query(contact);
-            // console.log('🆗 Tabla contact existe o fue creada con éxito');
+            console.log('🆗 Tabla contact existe o fue creada con éxito');
         } catch (error) {
-            console.error('🚫 Error al crear la tabla acontact:', error);
+            console.error('🚫 Error al crear la tabla contact:', error);
         }
 
         const history = `
@@ -131,7 +126,7 @@ class PostgreSQLAdapter {
             )`;
         try {
             await this.db.query(history);
-            // console.log('🆗 Tabla history existe o fue creada con éxito');
+            console.log('🆗 Tabla history existe o fue creada con éxito');
         } catch (error) {
             console.error('🚫 Error al crear la tabla de history:', error);
         }
@@ -140,43 +135,75 @@ class PostgreSQLAdapter {
     }
 
     createSP = async() => {
-        const sp = `
+        const sp_suc = `
+        CREATE OR REPLACE FUNCTION save_or_update_contact(
+            in_phone VARCHAR(255),
+            in_values JSONB
+        )
+        RETURNS VOID AS
+        $$
+        DECLARE
+            contact_cursor refcursor := 'cur_contact';
+            contact_id INT;
+        BEGIN
+            SELECT id INTO contact_id FROM contact WHERE phone = in_phone;
+        
+            IF contact_id IS NULL THEN
+                INSERT INTO contact (phone, "values")
+                VALUES (in_phone, in_values);
+            ELSE
+                UPDATE contact SET "values" = in_values, updated_in = current_timestamp
+                WHERE id = contact_id;
+            END IF;
+        END;
+        $$ LANGUAGE plpgsql;`
+
+        try {
+            await this.db.query(sp_suc);
+            console.log('🆗 Procedimiento almacenado de contacto existe o fue creada con éxito');
+        } catch (error) {
+            console.error('🚫 Error al crear el procedimiento almacenado de contacto:', error);
+        }
+
+        const sp_suhc = `
         CREATE OR REPLACE FUNCTION save_or_update_history_and_contact(
-            _ref VARCHAR(255),
-            _keyword VARCHAR(255),
-            _answer TEXT,
-            _refserialize TEXT,
-            _phone VARCHAR(255),
-            _options JSONB,
-            _contact_name VARCHAR(255)
+            in_ref VARCHAR(255),
+            in_keyword VARCHAR(255),
+            in_answer TEXT,
+            in_refserialize TEXT,
+            in_phone VARCHAR(255),
+            in_options JSONB
         )
         RETURNS VOID AS
         $$
         DECLARE
             _contact_id INT;
         BEGIN
-            SELECT id INTO _contact_id FROM contact WHERE phone = _phone;
+            SELECT id INTO _contact_id FROM contact WHERE phone = in_phone;
         
             IF _contact_id IS NULL THEN
-                INSERT INTO contact (name, phone, created_at)
-                VALUES (_contact_name, _phone, current_timestamp)
+                INSERT INTO contact (phone)
+                VALUES (in_phone)
                 RETURNING id INTO _contact_id;
             ELSE
                 UPDATE contact SET last_interaction = current_timestamp WHERE id = _contact_id;
             END IF;
         
             INSERT INTO history (ref, keyword, answer, refserialize, phone, options, contact_id, created_at)
-            VALUES (_ref, _keyword, _answer, _refserialize, _phone, _options, _contact_id, current_timestamp);
+            VALUES (in_ref, in_keyword, in_answer, in_refserialize, in_phone, in_options, _contact_id, current_timestamp);
         
         END;
         $$ LANGUAGE plpgsql;`;
 
         try {
-            await this.db.query(sp);
-            // console.log('🆗 Procedimiento almacenado existe o fue creada con éxito');
+            await this.db.query(sp_suhc);
+            console.log('🆗 Procedimiento almacenado de historico existe o fue creada con éxito');
         } catch (error) {
-            console.error('🚫 Error al crear el procedimiento almacenado:', error);
+            console.error('🚫 Error al crear el procedimiento almacenado de historico:', error);
         }
+
+        
+
     }
 }
 
