f2272a058b2fa34ab483c39287aa6b6d5f537837
Trystan4861 <trystan4861@gmail.com>
2023-08-29 10:36:37 +0200

    ✨ Added EVENT.BUTTON to EVENTS' LIST

--- packages/bot/io/events/index.js
+++ packages/bot/io/events/index.js
@@ -1,6 +1,7 @@
 const { eventDocument, REGEX_EVENT_DOCUMENT } = require('./eventDocument')
 const { eventLocation, REGEX_EVENT_LOCATION } = require('./eventLocation')
 const { eventMedia, REGEX_EVENT_MEDIA } = require('./eventMedia')
+const { eventButton, REGEX_EVENT_BUTTON} = require('./eventButton')
 const { eventVoiceNote, REGEX_EVENT_VOICE_NOTE } = require('./eventVoiceNote')
 const { eventWelcome } = require('./eventWelcome')
 const { eventAction } = require('./eventAction')
@@ -8,6 +9,7 @@ const { eventAction } = require('./eventAction')
 const LIST_ALL = {
     WELCOME: eventWelcome(),
     MEDIA: eventMedia(),
+    BUTTON: eventButton(),
     LOCATION: eventLocation(),
     DOCUMENT: eventDocument(),
     VOICE_NOTE: eventVoiceNote(),
@@ -18,6 +20,7 @@ const LIST_REGEX = {
     REGEX_EVENT_DOCUMENT,
     REGEX_EVENT_LOCATION,
     REGEX_EVENT_MEDIA,
+    REGEX_EVENT_BUTTON,
     REGEX_EVENT_VOICE_NOTE,
 }
 
