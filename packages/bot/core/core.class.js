966834eb54967348fbb0bb88653add8c968ad121
Carlos Moran <bl4ckdll@gmail.com>
2023-10-19 12:15:20 -0500

    fix(bot): :fire: se incorpora la funcionalidad de blacklist dinámica
    📢Ahora incluimos la blacklist dinámica en nuestro sistema. 🚫✨

--- packages/bot/core/core.class.js
+++ packages/bot/core/core.class.js
@@ -20,6 +20,7 @@ const loggerQueue = new Console({
 })
 
 const idleForCallback = new IdleState()
+const DynamicBlacklist = require('../utils/BlacklistDynamic')
 
 /**
  * [ ] Escuchar eventos del provider asegurarte que los provider emitan eventos
@@ -34,6 +35,7 @@ class CoreClass extends EventEmitter {
     queuePrincipal
     stateHandler = new SingleState()
     globalStateHandler = new GlobalState()
+    dynamicBlacklist = new DynamicBlacklist()
     generalArgs = {
         blackList: [],
         listEvents: {},
@@ -51,6 +53,8 @@ class CoreClass extends EventEmitter {
         this.databaseClass = _database
         this.providerClass = _provider
         this.generalArgs = { ...this.generalArgs, ..._args }
+
+        this.dynamicBlacklist.addToBlacklist(this.generalArgs.blackList)
 
         this.queuePrincipal = new Queue(
             loggerQueue,
@@ -108,7 +112,7 @@ class CoreClass extends EventEmitter {
         let msgToSend = []
         let endFlowFlag = false
         let fallBackFlag = false
-        if (this.generalArgs.blackList.includes(from)) return
+        if (this.dynamicBlacklist.isInBlacklist(from)) return
         if (!body) return
 
         let prevMsg = await this.databaseClass.getPrevByNumber(from)
