9ba11f97475aefc55660f38f4870027b3314172b
Leifer Mendez <leifer33@gmail.com>
2023-11-27 10:40:37 +0100

    build: :zap: improved

--- __test__/0.2.0-case.test.js
+++ __test__/0.2.0-case.test.js
@@ -94,7 +94,6 @@ suiteCase(`Encadenanos addAction con captures and gotoFlow`, async ({ database, 
 
     const flujoTercero = addKeyword(['ordenar'])
         .addAction({ ref: `00000000000000000000000` }, async (ctx, { flowDynamic, state }) => {
-            console.log(`🔴🔴🔴🔴`)
             await flowDynamic(`Gracias por el interes`)
         })
         .addAction({ ref: `1111111111111111111` }, async (ctx, { flowDynamic }) => {
