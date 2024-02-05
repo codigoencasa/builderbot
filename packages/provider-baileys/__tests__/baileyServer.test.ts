0da932994317b265bad13012eb8f24953b893c3f
vicente1992 <vic_ortiz20@hotmail.es>
2024-02-05 12:22:23 -0500

    refactor: unit test

--- packages/provider-baileys/__tests__/baileyServer.test.ts
+++ packages/provider-baileys/__tests__/baileyServer.test.ts
@@ -1,7 +1,16 @@
 import * as assert from 'assert'
 import { test } from 'uvu'
 
-import { BaileyHttpServer } from '../src/server'
+import { BaileyHttpServer, handleCtx } from '../src/server'
+
+const mockRequest = {}
+const mockResponse = {
+    status: 200,
+    data: '',
+    end: function (data: string) {
+        this.data = data
+    },
+}
 
 const baileyHttpServer = new BaileyHttpServer(3000)
 
@@ -15,6 +24,25 @@ test('start debe actualizar el puerto correctamente si se proporciona un valor',
     baileyHttpServer.start(undefined as any, 4000)
 
     assert.strictEqual(baileyHttpServer.port, 4000)
+})
+
+test('handleCtx - function should call provided function with correct arguments', () => {
+    const testFn = (__, req: any, res: any) => {
+        assert.equal(req, mockRequest)
+        assert.equal(res, mockResponse)
+        res.end('Test completed')
+    }
+
+    const handler = handleCtx(testFn)
+    handler(mockRequest, mockResponse)
+    assert.equal(mockResponse.data, 'Test completed')
+})
+
+test('stop method should close the server without error', async () => {
+    const server = new BaileyHttpServer(500)
+    server['start'](undefined as any)
+    await server.stop()
+    assert.equal(server.server.server?.listening, false)
 })
 
 test.run()
