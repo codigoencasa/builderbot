638c00b603ee6af0d2ad947b1280e1756e1c7725
vicente1992 <vic_ortiz20@hotmail.es>
2024-02-14 22:47:05 -0500

    refactor:  starters

--- starters/apps/base-baileys-memory/rollup.config.js
+++ starters/apps/base-baileys-memory/rollup.config.js
@@ -1,5 +1,5 @@
-import typescript from 'rollup-plugin-typescript2'
 import terser from '@rollup/plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
 
 export default {
     input: 'app.ts',
