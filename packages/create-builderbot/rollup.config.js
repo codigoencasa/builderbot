e8dff7280cc32cb3fb0298448fcc987cf4b628b7
Leifer Mendez <leifer33@gmail.com>
2024-03-07 14:51:43 +0100

    fix: cli and create script

--- packages/create-builderbot/rollup.config.js
+++ packages/create-builderbot/rollup.config.js
@@ -1,19 +1,21 @@
-import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
-
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 export default {
     input: ['src/index.ts'],
-    output: {
-        dir: 'dist',
-        entryFileNames: '[name].cjs',
-        format: 'cjs',
-        exports: 'named',
-    },
+    output: [
+        {
+            dir: 'dist',
+            entryFileNames: '[name].cjs',
+            format: 'cjs',
+            exports: 'named',
+        },
+    ],
     plugins: [
         commonjs(),
-        nodeResolve({ resolveOnly: ['!rpt2'] }),
+        nodeResolve({
+            resolveOnly: (module) => !/@builderbot\/cli|sharp/i.test(module),
+        }),
         typescript(),
-        // terser()
     ],
 }
