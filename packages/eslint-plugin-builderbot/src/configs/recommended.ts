7cdc1c14e7cfbc5703b447d58a5f15042941937e
Leifer Mendez <leifer33@gmail.com>
2024-03-13 16:29:30 +0100

    fix: templates

--- packages/eslint-plugin-builderbot/src/configs/recommended.ts
+++ packages/eslint-plugin-builderbot/src/configs/recommended.ts
@@ -1,8 +1,8 @@
 export const rulesRecommended = {
-    'bot-whatsapp/func-prefix-goto-flow-return': 2,
-    'bot-whatsapp/func-prefix-end-flow-return': 2,
-    'bot-whatsapp/func-prefix-dynamic-flow-await': 2,
-    'bot-whatsapp/func-prefix-state-update-await': 2,
-    'bot-whatsapp/func-prefix-fall-back-return': 2,
-    'bot-whatsapp/func-prefix-endflow-flowdynamic': 2,
+    'builderbot/func-prefix-goto-flow-return': 2,
+    'builderbot/func-prefix-end-flow-return': 2,
+    'builderbot/func-prefix-dynamic-flow-await': 2,
+    'builderbot/func-prefix-state-update-await': 2,
+    'builderbot/func-prefix-fall-back-return': 2,
+    'builderbot/func-prefix-endflow-flowdynamic': 2,
 }
