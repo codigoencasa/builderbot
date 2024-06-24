5aec50a61f6d51bb8d1753ac7827fb5e473364fb
leifermendez <leifer33@gmail.com>
2024-06-24 13:20:20 +0200

    feat: add whitelist

--- packages/bot/__tests__/units/blacklist.test.ts
+++ packages/bot/__tests__/units/blacklist.test.ts
@@ -1,16 +1,36 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-
 import { BlackList } from '../../src/utils/blacklistClass'
 
-test('constructor', () => {
+test('BlackList constructor initializes correctly', () => {
     const blacklist = new BlackList(['123', '456'])
     assert.ok(blacklist.checkIf('123'))
     assert.ok(blacklist.checkIf('456'))
     assert.not.ok(blacklist.checkIf('789'))
 })
 
-test('add', () => {
+test('BlackList avoid constructor initializes correctly with multiple numbers', () => {
+    const blacklist = new BlackList(['!123', '456', '!789'])
+    assert.not.ok(blacklist.checkIf('123'))
+    assert.ok(blacklist.checkIf('456'))
+    assert.not.ok(blacklist.checkIf('789'))
+})
+
+test('BlackList avoid constructor initializes correctly with mixed numbers', () => {
+    const blacklist = new BlackList(['!123', '456', '789', '!101'])
+    assert.not.ok(blacklist.checkIf('123'))
+    assert.ok(blacklist.checkIf('456'))
+    assert.ok(blacklist.checkIf('789'))
+    assert.not.ok(blacklist.checkIf('101'))
+})
+
+test('BlackList avoid constructor initializes correctly with only avoid numbers', () => {
+    const blacklist = new BlackList(['!123', '!456'])
+    assert.not.ok(blacklist.checkIf('123'))
+    assert.not.ok(blacklist.checkIf('456'))
+})
+
+test('BlackList add method works correctly', () => {
     const blacklist = new BlackList()
     let response = blacklist.add('123')
     assert.equal(response, ['Número 123 añadido exitosamente.'])
@@ -20,7 +40,7 @@ test('add', () => {
     assert.equal(response, ['El número de teléfono 123 ya está en la lista negra.'])
 })
 
-test('remove', () => {
+test('BlackList remove method works correctly', () => {
     const blacklist = new BlackList(['123'])
     assert.ok(blacklist.checkIf('123'))
 
@@ -35,19 +55,19 @@ test('remove', () => {
     }
 })
 
-test('checkIf', () => {
+test('BlackList checkIf method works correctly', () => {
     const blacklist = new BlackList(['123'])
     assert.ok(blacklist.checkIf('123'))
     assert.not.ok(blacklist.checkIf('456'))
 })
 
-test('getList', () => {
+test('BlackList getList method works correctly', () => {
     const blacklist = new BlackList(['123', '456'])
     const list = blacklist.getList()
     assert.equal(list, ['123', '456'])
 })
 
-test('PhoneNumberAlreadyExistsError', () => {
+test('BlackList handles PhoneNumberAlreadyExistsError correctly', () => {
     const blacklist = new BlackList(['123'])
     const response = blacklist.add('123')
     assert.equal(response, ['El número de teléfono 123 ya está en la lista negra.'])
